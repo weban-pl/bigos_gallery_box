5dedfc3403781e7d0ec8529674815b16943eaee7
kremenso <kremenso@o2.pl>
2013-02-08 17:32:26 +0100

    gallery images attributes

--- app/models/bigos_gallery_box/gallery.rb
+++ app/models/bigos_gallery_box/gallery.rb
@@ -1,6 +1,6 @@
 module BigosGalleryBox
   class Gallery < ActiveRecord::Base
-    attr_accessible :bigos_gallery_element_id, :is_background, :title
+    attr_accessible :bigos_gallery_element_id, :is_background, :title, :gallery_images_attributes
     has_many :gallery_images, :dependent => :destroy
 
     accepts_nested_attributes_for :gallery_images
